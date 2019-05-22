c77702a2a3f76f6c1bd34a6f5625dc49380d999c
Ming Duan <11203duan@gmail.com>
2019-05-22 02:23:02 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -17,9 +17,10 @@ class PigLatinizer
     elsif second_letter != "a" || second_letter != "e" || second_letter != "i" || second_letter != "o" || second_letter != "u"
 
       word_arr.shift
+      binding.pry
       phrase = word_arr.shift.join("") + first_letter + second_letter + "ay"
     else phrase = word_arr.shift.join("") + first_letter + "ay"
-binding.pry
+
     end
   end
 
