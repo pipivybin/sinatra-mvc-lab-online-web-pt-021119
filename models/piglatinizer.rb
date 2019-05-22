c9f01ee26e75989e29c7034d12d872b39adcf990
Ming Duan <11203duan@gmail.com>
2019-05-22 02:09:08 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -12,7 +12,6 @@ class PigLatinizer
       word_arr = word.split("")
     first_letter = word_arr[0]
     second_letter = word_arr[1]
-binding.pry
     if first_letter == a || e || i || o || u
       phrase = word_arr.join("") + "way"
     elsif second_letter != a || e || i || o || u
