cf3578abb737761916cd701322288db3a5ba72d3
Ming Duan <11203duan@gmail.com>
2019-05-22 03:12:48 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,5 +1,11 @@
 class PigLatinizer
 
+  def consonant?(text)
+    !char.match(/[aAeEiIoOuU]/)
+  end
+    
+  end
+
   def piglatinize(text)
     arr = text.split(" ")
     output = []
@@ -8,7 +14,7 @@ class PigLatinizer
     first_letter = word_arr[0].downcase
     second_letter = word_arr[1]
     third_letter = word_arr[2]
-    if  ["a", "e", "i", "o", "u"].include?(first_letter)
+    if  !consonant
       phrase = word_arr.join("") + "way"
     elsif (second_letter || third_letter) != "a" && (second_letter || third_letter) != "e" && (second_letter || third_letter) != "i" && (second_letter || third_letter) != "o" && (second_letter || third_letter) != "u"
       word_arr.shift
