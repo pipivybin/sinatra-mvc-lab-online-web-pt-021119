ec0a411e013a0e2114f4562dc94cd89ae3201511
Ming Duan <11203duan@gmail.com>
2019-05-22 02:18:13 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -14,7 +14,7 @@ class PigLatinizer
     second_letter = word_arr[1]
     if  ["a", "e", "i", "o", "u"].include?(first_letter)
       phrase = word_arr.join("") + "way"
-    elsif second_letter != a || e || i || o || u
+    elsif second_letter != "a" || second_letter != "e" || second_letter != "i" || second_letter != "o" || second_letter != "u"
       binding.pry
       phrase = word_arr.shift.shift.join("") + first_letter + second_letter + "ay"
     else phrase = word_arr.shift.join("") + first_letter + "ay"
