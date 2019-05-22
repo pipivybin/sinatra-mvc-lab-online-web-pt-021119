7ab80092c11958d9ed33c478e3b78667026bfbd4
Ming Duan <11203duan@gmail.com>
2019-05-22 01:44:30 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,6 +7,7 @@ class PigLatinizer
   def piglatinize
     arr = @text.split(" ")
     arr.collect do |word|
+      output = []
       word_arr = word.split("")
     first_letter = word_arr.first
     arr_new = word_arr.shift << first_letter
