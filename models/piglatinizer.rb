dc9b25db09e6a3147f2869a39dc636577d4e7528
Ming Duan <11203duan@gmail.com>
2019-05-22 02:57:10 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -18,7 +18,6 @@ class PigLatinizer
       word_arr.shift
       word_arr.shift
       word_arr.shift
-      word_arr.shift
       phrase = word_arr.join("") + first_letter + second_letter + third_letter + "ay"
     else word_arr.shift
       phrase = word_arr.join("") + first_letter + "ay"
