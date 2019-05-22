f8173b5dab0ecccbad03b5d9ecc90e078e344273
Ming Duan <11203duan@gmail.com>
2019-05-22 02:52:04 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,14 +2,13 @@ class PigLatinizer
 
   def piglatinize(text)
     arr = text.split(" ")
+    output = []
     arr.collect do |word|
-      output = []
       word_arr = word.split("")
     first_letter = word_arr[0].downcase
     second_letter = word_arr[1]
     if  ["a", "e", "i", "o", "u"].include?(first_letter)
       phrase = word_arr.join("") + "way"
-
     elsif second_letter != "a" && second_letter != "e" && second_letter != "i" && second_letter != "o" && second_letter != "u"
       word_arr.shift
       word_arr.shift
@@ -17,11 +16,8 @@ class PigLatinizer
     else word_arr.shift
       phrase = word_arr.join("") + first_letter + "ay"
     end
-
     output << phrase
-    
   end
-
   output.join(", ")
   end
 
