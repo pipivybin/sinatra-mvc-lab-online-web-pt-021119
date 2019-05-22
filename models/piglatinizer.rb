6e6760086e92751ef2a264cc81e3b8378809d317
Ming Duan <11203duan@gmail.com>
2019-05-22 01:54:28 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -13,7 +13,7 @@ class PigLatinizer
     first_letter = word_arr.first
     no_first_letter = word_arr.shift
     no_first_letter << first_letter
-     
+
     binding.pry
     output << arr_new.join
     end
